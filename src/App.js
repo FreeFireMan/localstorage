d9962290014053dafa9994d0e6d87364b3e58afe
NataliiaRybalka <natalirybalka21@gmail.com>
2021-04-20 22:01:40 +0300

    31.03hwDZ

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
 import './App.css';
 
+/*
+створти 2 інтупи і кнопку
+перший відповідає за ендпоінт джсон плейсхолдера (перша частина енпоніту) другий- за айдішнік  якщо другого
+ендпоінту нема- тягнемо весь список  потрібно зробити валідацію на перший інпут- чи ендпоінт існуючий на
+другий- чи це число і чи воно в рамках 1-100  зробити версію на функціональній компоненті контрольовану і
+не контрольовану  якщо є час- на класовій компоненті теж таке саме написати
+*/
+/*
+inputs (for each create 2 versions- class based and functional based)
+create controlled and uncontrolled select component
+create controlled and uncontrolled checkbox
+create controlled and uncontrolled radio
+ */
+
 function App() {
 
   return (
