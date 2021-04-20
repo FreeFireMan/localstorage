05304aa8e402a5b89ebae02eb4c4779f4cd63023
NataliiaRybalka <natalirybalka21@gmail.com>
2021-04-20 14:12:48 +0300

    24.03start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import {ProductCard} from "./components/Card/Card";
 
 function App() {
   return (
     <div className="App">
-        <ProductCard title={'title1'} price={'$19.99'} descr={'descr1'} />
-        <hr />
-        <ProductCard title={'title2'} price={''} descr={'descr2'} />
-        <hr />
-        <ProductCard title={'title3'} price={'$39.99'} />
-        <hr />
-        <ProductCard title={'title4'} price={'$49.99'} descr={'descr4'} />
-        <hr />
+
     </div>
   );
 }
