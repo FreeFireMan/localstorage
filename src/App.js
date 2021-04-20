16bf59e7ec3c0c546e88c2e484029acdcfb5439d
NataliiaRybalka <natalirybalka21@gmail.com>
2021-04-20 15:25:30 +0300

    24.03hwStart

--- src/App.js
+++ src/App.js
@@ -1,108 +1,23 @@
-import React, {Component, useState} from "react";
+import React from "react";
 import './App.css';
 
-const Header = ({counter}) => {
+/*
+ 1 відмалювати список карточок базуючись на якомусь створеному вами масиві створити окрему кнопку, яка буде видаляти поточний перший елемент (або останній)  якщо у нас масив з 3 елементів і ми клікнули на кнопку 3 рази, то на екрані жодна карточка не має відмалюватись  (кнопки повернення до початкового стану не треба)
 
-  return (
-      <h2>{counter}</h2>
-  );
-}
+  2 те саме, тільки з кнопкою реверт (повернутись до стану, де у нас видно 3 елемнети, як на початку)
 
-const TodoItem = ({todo}) => {
-  return (
-    <div>
-      {todo.title} - {todo.descr} <hr />
-    </div>
-  );
-}
+3   задача з зірочкою) кожна карточка з завдання вище має мати кнопку, по кліку на яку, ми видаляємо зі списку саме її + реверт кнопка, щоб вернути все назад (ця кнопка одна дня всіх карточок, клікнули по ній і всі каркти вернулись назазд) (згадування функції фільтр в лекції було не просто так)
+
+4   написати тогл компоненту, яка буде ховати або показувати елемент приклад з візуалкою тут https://material-ui.com/components/switches/#customized-switches
+*/
 
 function App() {
-   const [state, setState] = useState({
-    counter: 0,
-    add: 3,
-    isHeaderVisible: true,
-    todos: [
-      {id: 1, title: 'Hello', descr: 'React'},
-      {id: 2, title: 'Bye', descr: 'React'},
-      {id: 3, title: 'Test', descr: 'React'},
-    ],
-  })
-
-  const incHandler = () => {
-    setState({
-      ...state,
-      counter: state.counter + 1 + state.add,
-      add: state.add + 2,
-    })
-  }
-
-  const toggleHeader = () => {
-    setState({
-      ...state,
-      isHeaderVisible: !state.isHeaderVisible,
-    })
-  }
-
-  const changeSmthInTodo = () => {
-    let newArr = [...state.todos];
-    newArr[0].title = Math.random();
-    setState({
-      ...state,
-      todos: newArr,
-    })
-  }
-
-  const {isHeaderVisible, counter, todos} = state;
 
   return (
     <div className="App">
-        {isHeaderVisible && <Header counter={counter}/>}
 
-      <button onClick={incHandler}>inc</button>
-      <button onClick={toggleHeader}>toggle header</button>
-      <button onClick={changeSmthInTodo}>change smth in todo</button>
-
-      {todos.map(value => <TodoItem key={value.id} todo={value} />)}
     </div>
   );
 }
-
-// class App extends Component {
-//   state = {
-//     counter: 0,
-//     isHeaderVisible: true,
-//     todos: ['hello', 'react'],
-//   }
-//
-//   incHandler = () => {
-//     this.setState({counter: this.state.counter + 1});
-//   }
-//
-//   toggleHeader = () => {
-//     this.setState({isHeaderVisible: !this.state.isHeaderVisible});
-//   }
-//
-//   changeSmthInTodo = () => {
-//     let newArr = [...this.state.todos];
-//     newArr[0] = Math.random();
-//     this.setState({todos: newArr});
-//   }
-//
-//   render() {
-//     const {isHeaderVisible, counter, todos} = this.state;
-//     return (
-//       <div className="App">
-//           {isHeaderVisible && <Header counter={counter}/>}
-//
-//         <button onClick={this.incHandler}>inc</button>
-//         <button onClick={this.toggleHeader}>toggle header</button>
-//         <button onClick={this.changeSmthInTodo}>change smth in todo</button>
-//
-//         <div>{todos[0]}</div>
-//         <div>{todos[1]}</div>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
